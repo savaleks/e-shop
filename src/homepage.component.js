6e870ed059fa55d7a24c78f00e5a1f2371f84c60
savaleks <savaleks@gmail.com>
2020-04-14 07:50:04 +0300

    added sass package and created homepage file

---
+++ src/homepage.component.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import "./homepage.style.scss"
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Item1</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Item2</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Item3</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Item4</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Item5</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Item6</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;

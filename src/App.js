08411aafd80629b53cb4f507ad1c8a47627ac39a
savaleks <savaleks@gmail.com>
2020-04-15 11:29:15 +0300

    added data for testing and preview collection component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const Item1Page = () => (
-  <div>
-    <h1>Item1 page</h1>
-  </div>
-)
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+    <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/item1' component={Item1Page}/>
+      <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,295 @@
+import cover from "../../img/cover.png";
+
+const SHOP_DATA = [
+    {
+      id: 1,
+      title: 'Item1',
+      routeName: 'phones',
+      items: [
+        {
+          id: 1,
+          name: 'Samsung',
+          imageUrl: cover,
+          price: 250
+        },
+        {
+          id: 2,
+          name: 'Nokia',
+          imageUrl: cover,
+          price: 180
+        },
+        {
+          id: 3,
+          name: 'Huawei',
+          imageUrl: cover,
+          price: 350
+        },
+        {
+          id: 4,
+          name: 'Motorola',
+          imageUrl: cover,
+          price: 250
+        },
+        {
+          id: 5,
+          name: 'Apple',
+          imageUrl: cover,
+          price: 180
+        },
+        {
+          id: 6,
+          name: 'LG',
+          imageUrl: cover,
+          price: 140
+        },
+        {
+          id: 7,
+          name: 'Sony',
+          imageUrl: cover,
+          price: 180
+        },
+        {
+          id: 8,
+          name: 'Xiaomi',
+          imageUrl: cover,
+          price: 140
+        },
+        {
+          id: 9,
+          name: 'HTC',
+          imageUrl: cover,
+          price: 160
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Item2',
+      routeName: 'batteries',
+      items: [
+        {
+          id: 10,
+          name: 'Alcaline',
+          imageUrl: cover,
+          price: 22
+        },
+        {
+          id: 11,
+          name: 'Siemens',
+          imageUrl: cover,
+          price: 28
+        },
+        {
+          id: 12,
+          name: 'Duracell',
+          imageUrl: cover,
+          price: 11
+        },
+        {
+          id: 13,
+          name: 'Energizer',
+          imageUrl: cover,
+          price: 16
+        },
+        {
+          id: 14,
+          name: 'Maxell',
+          imageUrl: cover,
+          price: 16
+        },
+        {
+          id: 15,
+          name: 'AmazonBasic',
+          imageUrl: cover,
+          price: 16
+        },
+        {
+          id: 16,
+          name: 'GloFX',
+          imageUrl: cover,
+          price: 19
+        },
+        {
+          id: 17,
+          name: 'LiCB',
+          imageUrl: cover,
+          price: 20
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Item3',
+      routeName: 'headphones',
+      items: [
+        {
+          id: 18,
+          name: 'COWIN',
+          imageUrl: cover,
+          price: 125
+        },
+        {
+          id: 19,
+          name: 'Bose',
+          imageUrl: cover,
+          price: 90
+        },
+        {
+          id: 20,
+          name: 'Apple',
+          imageUrl: cover,
+          price: 90
+        },
+        {
+          id: 21,
+          name: 'Sony',
+          imageUrl: cover,
+          price: 165
+        },
+        {
+          id: 22,
+          name: 'Panasonic',
+          imageUrl: cover,
+          price: 185
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Item4',
+      routeName: 'laptop',
+      items: [
+        {
+          id: 23,
+          name: 'HP',
+          imageUrl: cover,
+          price: 2500
+        },
+        {
+          id: 24,
+          name: 'Apple',
+          imageUrl: cover,
+          price: 2000
+        },
+        {
+          id: 25,
+          name: 'Dell',
+          imageUrl: cover,
+          price: 800
+        },
+        {
+          id: 26,
+          name: 'Toshiba',
+          imageUrl: cover,
+          price: 800
+        },
+        {
+          id: 27,
+          name: 'Acer',
+          imageUrl: cover,
+          price: 4500
+        },
+        {
+          id: 28,
+          name: 'Asus',
+          imageUrl: cover,
+          price: 1350
+        },
+        {
+          id: 29,
+          name: 'Lenovo',
+          imageUrl: cover,
+          price: 2000
+        }
+      ]
+    },
+    {
+      id: 5,
+      title: 'Item5',
+      routeName: 'cables',
+      items: [
+        {
+          id: 30,
+          name: 'Nordline',
+          imageUrl: cover,
+          price: 32
+        },
+        {
+          id: 31,
+          name: 'LIne',
+          imageUrl: cover,
+          price: 20
+        },
+        {
+          id: 32,
+          name: 'DoPro',
+          imageUrl: cover,
+          price: 25
+        },
+        {
+          id: 33,
+          name: 'LandLine',
+          imageUrl: cover,
+          price: 25
+        },
+        {
+          id: 34,
+          name: 'Siemens',
+          imageUrl: cover,
+          price: 40
+        },
+        {
+          id: 35,
+          name: 'SecureLine',
+          imageUrl: cover,
+          price: 25
+        }
+      ]
+    },
+    {
+      id: 6,
+      title: 'Item6',
+      routeName: 'cameras',
+      items: [
+        {
+          id: 30,
+          name: 'Nikon',
+          imageUrl: cover,
+          price: 325
+        },
+        {
+          id: 31,
+          name: 'Sony',
+          imageUrl: cover,
+          price: 200
+        },
+        {
+          id: 32,
+          name: 'Olympus',
+          imageUrl: cover,
+          price: 250
+        },
+        {
+          id: 33,
+          name: 'GoPro',
+          imageUrl: cover,
+          price: 250
+        },
+        {
+          id: 34,
+          name: 'FujiFilm',
+          imageUrl: cover,
+          price: 400
+        },
+        {
+          id: 35,
+          name: 'Canon',
+          imageUrl: cover,
+          price: 250
+        }
+      ]
+    }
+  ];
+  
+  export default SHOP_DATA;
+  

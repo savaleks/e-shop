acb07e83778ed584cd0a8d721aa3b492681940ba
savaleks <savaleks@gmail.com>
2020-04-18 23:21:16 +0300

    added sign-in, custom-button, form-input pages

---
+++ src/pages/sign-page/sign-page.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./sign-page.style.scss";
+
+const SignPage = () => (
+    <div className="sign-page">
+        <SignIn/>
+    </div>
+)
+
+export default SignPage;

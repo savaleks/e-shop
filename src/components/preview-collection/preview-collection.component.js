fd9608753422c5a72b006283aae9c445eb4d82ad
savaleks <savaleks@gmail.com>
2020-04-18 21:11:59 +0300

    updated shop page and other

--- src/components/preview-collection/preview-collection.component.js
+++ src/components/preview-collection/preview-collection.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./preview-collection.style.scss";
+import ItemCollection from "../item-collection/item-collection.component";
 
 const PreviewCollection = ({title, items}) => (
     <div className="collection-preview">
@@ -8,8 +9,8 @@ const PreviewCollection = ({title, items}) => (
             {
                 items
                 .filter((item, index) => index < 4)
-                .map((item) => (
-                   <div key={item.id}>{item.name}</div> 
+                .map(({id, ...otherItemProps}) => (
+                   <ItemCollection key={id} {...otherItemProps} />
                 ))
             }
         </div>
